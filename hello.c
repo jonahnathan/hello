866506ba9ad63c48087c41bc4a2c4f94573deb61
Jonah Nathan <jnathan4@huskers.unl.edu>
2022-01-21 23:39:58 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jonah Nathan
+ * Date: 2022
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Hello World!\n");
+
+     return 0;
+ }
